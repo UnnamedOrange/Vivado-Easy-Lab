57451db3cb1235043be5490a9c96e404009c8b9d
UnnamedOrange <lycheng1215@sina.com>
2021-05-14 17:06:45 +0800

    Implement heater_t.

--- assignment6/assignment6.srcs/sources_1/new/heater.v
+++ assignment6/assignment6.srcs/sources_1/new/heater.v
@@ -9,12 +9,67 @@
 
 // Three-section coding style.
 module heater_t_imp1(
-    output HEATING_STATUS,
-    output DONE_STATUS,
+    output reg HEATING_STATUS,
+    output reg DONE_STATUS,
     input BUTTON,
     input TEMPERATURE,
     input CLK,
     input RESET);
 
+    parameter [1:0] status_ready = 2'b00;
+    parameter [1:0] status_heating = 2'b01;
+    parameter [1:0] status_finished = 2'b11;
+    
+    reg [1:0] crt, next;
 
+    always @(posedge CLK) begin
+        if (RESET)
+            crt <= status_ready;
+        else
+            crt <= next;
+    end
+
+    always @(BUTTON, TEMPERATURE, crt) begin
+        case (crt)
+            status_ready:
+                if (BUTTON)
+                    next = status_heating;
+                else
+                    next = status_ready;
+            status_heating:
+                if (TEMPERATURE)
+                    next = status_finished;
+                else
+                    next = status_heating;
+            status_finished:
+                if (BUTTON)
+                    next = status_ready;
+                else
+                    next = status_finished;
+            default:
+                next = status_ready;
+        endcase
+    end
+
+    always @(BUTTON, TEMPERATURE, crt) begin
+        case (crt)
+            status_ready:
+                if (BUTTON)
+                    {HEATING_STATUS, DONE_STATUS} = 2'b10;
+                else
+                    {HEATING_STATUS, DONE_STATUS} = 2'b00;
+            status_heating:
+                if (TEMPERATURE)
+                    {HEATING_STATUS, DONE_STATUS} = 2'b01;
+                else
+                    {HEATING_STATUS, DONE_STATUS} = 2'b10;
+            status_finished:
+                if (BUTTON)
+                    {HEATING_STATUS, DONE_STATUS} = 2'b00;
+                else
+                    {HEATING_STATUS, DONE_STATUS} = 2'b01;
+            default:
+                {HEATING_STATUS, DONE_STATUS} = 2'b00;
+        endcase
+    end
 endmodule
